2948ff240aa5901339fd20da0b89b2b0fc3d8385
Pablo <kovalwaffe@gmail.com>
2016-05-08 18:27:22 +0200

    warunek sprawdzenia formularza JS

--- day9/js/program.js
+++ day9/js/program.js
@@ -1,15 +1,46 @@
-//wszystko w JS jest obiektem
 'use strict';
-console.log("Dziala");
 
-function welcome() {
-    // do zmiennej "imie" przypisuje wartosc dokumentu->forms->id formatki->zmiennej=name->odczytajnie wartosci z tej zmiennej "name"
-    var imie = document.forms.formularz.name.value;
+function check() {
+    //zmienna "formularz" odpowiada na wyslanie formularza (submit)
+    var formularz = document.forms['formularz'];
+
+    var login = document.forms.formularz.login.value;
+    var pass = document.forms.formularz.pass.value;
+
+    var komunikatLogin = document.getElementById('errorlogin');
+    var komunikatPass = document.getElementById('errorpass');
+
+    var blad = false;
 
 
-    if (imie == "") {
-        window.alert("Wpisza imie");
-    } else {
-        window.alert("Witaj " + imie);
+
+
+
+
+
+
+
+
+
+    if((login == "") && (pass == "")){
+        komunikatLogin.innerHTML = "Nie podałeś loginu !";
+        komunikatPass.innerHTML = "Nie podałeś hasła !";
+         blad = true;
+    }else if (login == "") {
+        komunikatLogin.innerHTML = "Nie podałeś loginu !";
+        komunikatPass.innerHTML = "";
+        blad = true;
+    }else if (pass == "") {
+        komunikatPass.innerHTML = "Nie podałeś hasła !";
+        komunikatLogin.innerHTML = "";
+        blad = true;
     }
+
+    if (blad) {
+        // sa bledy blad = TRUE
+    }else{
+        //submit wysyla dane
+         formularz.submit();
+    }
+
 }
